c7554a2f19fa6c622c740434e00ad5d905f21d57
fancy_gen <107956435+xiaoguanghuan123@users.noreply.github.com>
2025-12-11 19:49:22 +0800

    Create init.py

---
+++ qtlprimer/init.py
@@ -0,0 +1,6 @@
+"""QTL Primer Toolkit - A comprehensive toolkit for designing PCR primers."""
+
+__version__ = "1.0.0"
+__author__ = "Your Name"
+__email__ = "your.email@unl.edu"
+__license__ = "MIT"
